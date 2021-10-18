332d28f83a69eed02384d32ac7b6bfdc31678a85
Amens <kill_nya@mail.ru>
2021-10-18 23:02:22 +0300

    a

--- build/tb_gazebo_plugins/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/user/catkin_ws/src/tb_gazebo_plugins/include".split(';') if "/home/user/catkin_ws/src/tb_gazebo_plugins/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "gazebo_ros;gazebo_plugins;geometry_msgs;kobuki_msgs;nav_msgs;roscpp;sensor_msgs;std_msgs;tf".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lgazebo_ros_kobuki".split(';') if "-lgazebo_ros_kobuki" != "" else []
-PROJECT_NAME = "tb_gazebo_plugins"
-PROJECT_SPACE_DIR = "/home/user/catkin_ws/devel/.private/tb_gazebo_plugins"
-PROJECT_VERSION = "0.5.7"
